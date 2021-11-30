83bbf1325d336f3e6783a608c90bc9f7496870c5
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-12-01 06:35:08 +0700

    wrap app with query client provider

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
-import { NavigationContainer } from '@react-navigation/native'
-import React from 'react'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
-import AppRoute from './src/routes/AppRoute'
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import AppRoute from './src/routes/AppRoute';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-    <NavigationContainer>
-      <AppRoute/>
-    </NavigationContainer>
-    </SafeAreaProvider>
-  )
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <AppRoute />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </QueryClientProvider>
+  );
 }
-
