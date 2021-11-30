8b6b970730960dce510f558362a0302d6a8a7bff
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-12-01 06:30:20 +0700

    set home screen header to logo title and make it center

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,13 +1,20 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import {LogoTitle} from '../components';
 import {AllCategoriesScreen, AllProductScreen, HomeScreen} from '../screens';
 
 const {Navigator, Screen} = createNativeStackNavigator();
 
 export default function AppRoute() {
   return (
-    <Navigator initialRouteName="home">
-      <Screen component={HomeScreen} name="home" />
+    <Navigator
+      initialRouteName="home"
+      screenOptions={{headerTitleAlign: 'center'}}>
+      <Screen
+        component={HomeScreen}
+        name="home"
+        options={{headerTitle: props => <LogoTitle {...props} />}}
+      />
       <Screen component={AllProductScreen} name="allProduct" />
       <Screen component={AllCategoriesScreen} name="allCategories" />
     </Navigator>
