db94f35423c00a263821b09742652111848bc665
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-12-01 06:20:40 +0700

    set home as initial route

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,18 +1,15 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { AllCategoriesScreen, AllProductScreen, HomeScreen } from '../screens'
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {AllCategoriesScreen, AllProductScreen, HomeScreen} from '../screens';
 
-const {Navigator, Screen} = createNativeStackNavigator()
+const {Navigator, Screen} = createNativeStackNavigator();
 
 export default function AppRoute() {
-    return (
-        <Navigator>
-            <Screen component={HomeScreen} name="home" />
-            <Screen component={AllProductScreen} name="allProduct" />
-            <Screen component={AllCategoriesScreen} name="allCategories"/>
-        </Navigator>
-    )
+  return (
+    <Navigator initialRouteName="home">
+      <Screen component={HomeScreen} name="home" />
+      <Screen component={AllProductScreen} name="allProduct" />
+      <Screen component={AllCategoriesScreen} name="allCategories" />
+    </Navigator>
+  );
 }
-
-const styles = StyleSheet.create({})
