9abb64b00b5e94331c2f13689d29d6c3016a3abc
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-12-01 09:08:08 +0700

    finished all categories screen

--- src/screens/AllCategoriesScreen.tsx
+++ src/screens/AllCategoriesScreen.tsx
@@ -1,12 +1,52 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, SafeAreaView, FlatList} from 'react-native';
+import {CategoryCard} from '../components';
+import {useCategories} from '../services/queries';
+import {size} from '../utils/size';
 
 export function AllCategoriesScreen() {
-    return (
-        <View>
-            <Text>All Categories</Text>
-        </View>
-    )
+  const {data: categories} = useCategories();
+
+  const renderCategories = ({item}) => (
+    <CategoryCard {...item} categoryCardStyle={styles.card} />
+  );
+
+  return (
+    <SafeAreaView style={styles.screen}>
+      <FlatList
+        data={categories.data}
+        renderItem={renderCategories}
+        numColumns={3}
+        contentContainerStyle={styles.contentContainer}
+      />
+      {/* {categories.data.map(item => (
+        <CategoryCard
+          {...item}
+          key={item.category_id}
+          categoryCardStyle={{
+            width: size(96),
+            height: size(96),
+            backgroundColor: 'red',
+          }}
+        />
+      ))} */}
+    </SafeAreaView>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  card: {
+    width: size(96),
+    height: size(96),
+    margin: size(8),
+  },
+  contentContainer: {
+    paddingTop: size(12),
+  },
+});
