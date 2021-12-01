c5a4464e436ebe92b76576b0ee7c6fa00f4824ba
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-12-01 08:42:25 +0700

    show only 6 first data, set category card margin

--- src/components/CategorySection.tsx
+++ src/components/CategorySection.tsx
@@ -5,21 +5,32 @@ import {CategoryCard} from './CategoryCard';
 import {HomeSectionTitle} from './HomeSectionTitle';
 
 export function CategorySection({data}) {
-  const renderCategories = ({item}) => <CategoryCard {...item} />;
+  const renderCategories = ({item}) => (
+    <CategoryCard {...item} categoryCardStyle={styles.card} />
+  );
   return (
-    <>
+    <View>
       <HomeSectionTitle
         title="Kategori"
         onPressSeeAll={() => console.log('Tes')}
       />
       <FlatList
         horizontal
-        data={data.slice(0, 8)}
+        data={data.slice(0, 6)}
         renderItem={renderCategories}
         overScrollMode="never"
+        contentContainerStyle={styles.sectionContainer}
       />
-    </>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    marginLeft: size(12),
+    marginVertical: size(8),
+  },
+  sectionContainer: {
+    paddingRight: size(12),
+  },
+});
