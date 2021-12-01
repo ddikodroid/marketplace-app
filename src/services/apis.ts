09e7376773022315fc03ab69bd5126322f17254f
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-12-01 08:35:29 +0700

    add fetchCategories function

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -4,3 +4,10 @@ export const fetchBanners = async () => {
   const res = await fetch(`${BASE_URL}/utility/home/banner-web`);
   return res.json();
 };
+
+export const fetchCategories = async () => {
+  const res = await fetch(
+    `${BASE_URL}/utility/home/box-category?with_staple=true`,
+  );
+  return res.json();
+};
