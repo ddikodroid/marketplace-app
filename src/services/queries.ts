c2e056fc4d49f2af259125807aea5fe8108b2412
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-12-01 09:18:05 +0700

    create `useProduct` and useInfiniteProducts`

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,5 +1,13 @@
-import {useQuery} from 'react-query';
-import {fetchBanners} from './apis';
-import {fetchCategories} from './apis';
+import {useInfiniteQuery, useQuery} from 'react-query';
+import {
+  fetchBanners,
+  fetchProducts,
+  fetchProduct,
+  fetchCategories,
+} from './apis';
+
 export const useBanners = () => useQuery('banners', fetchBanners);
 export const useCategories = () => useQuery('categories', fetchCategories);
+export const useProducts = () => useQuery('products', fetchProduct);
+export const useInfiniteProducts = () =>
+  useInfiniteQuery('infiniteProducts', fetchProducts);
