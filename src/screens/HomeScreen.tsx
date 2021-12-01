a9a432e40b353cdce1fac5444244e7849ebf334a
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-12-01 07:32:59 +0700

    display banner on home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, SafeAreaView} from 'react-native';
+import {useQuery} from 'react-query';
+import {BannerSection} from '../components';
+import {fetchBanners} from '../services/apis';
 
 export function HomeScreen() {
-    return (
-        <View>
-            <Text>HomeScreen</Text>
-        </View>
-    )
+  const {data: banners} = useQuery('banners', fetchBanners);
+  console.log(banners);
+  return (
+    <SafeAreaView style={styles.screen}>
+      <BannerSection data={banners.data} />
+    </SafeAreaView>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
