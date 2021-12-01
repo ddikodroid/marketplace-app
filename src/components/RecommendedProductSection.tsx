40ef19c63e049de3215db2877ac8643e7dfba11b
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-12-01 11:06:38 +0700

    create recommended product section

---
+++ src/components/RecommendedProductSection.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {size} from '../utils/size';
+import {CategoryCard} from './CategoryCard';
+import {HomeSectionTitle} from './HomeSectionTitle';
+import {ProductCard} from './ProductCard';
+
+export function RecommendedProductSection({data, onPressSeeAll}) {
+  const renderProduct = ({item}) => (
+    <ProductCard {...item} productCardStyle={styles.card} />
+  );
+  return (
+    <View>
+      <HomeSectionTitle
+        title="Rekomendasi Produk"
+        onPressSeeAll={onPressSeeAll}
+      />
+      <FlatList
+        horizontal
+        data={data.slice(0, 6)}
+        renderItem={renderProduct}
+        keyExtractor={(item, index) => `${item}-${index}`}
+        overScrollMode="never"
+        contentContainerStyle={styles.sectionContainer}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    marginLeft: size(12),
+    marginVertical: size(8),
+  },
+  sectionContainer: {
+    paddingRight: size(12),
+  },
+});
